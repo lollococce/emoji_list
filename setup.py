4e5747a0c9341ba5250e340adf0a5999b9f0d605
Lorenzo Coacci <30072155+lollococce@users.noreply.github.com>
2021-03-20 01:42:58 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="emojis_list",
+    name="emojis",
     version="0.1",
     author="Lorenzo Coacci",
     author_email="lorenzo@coacci.com",
-    description="The package contains an updated list of emoji",
+    description="The package contains an list of all emojis",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/lollococce/emoji_list",
+    url="https://github.com/lollococce/emojis",
     packages=setuptools.find_packages(),
     keywords='',
     license='MIT',
